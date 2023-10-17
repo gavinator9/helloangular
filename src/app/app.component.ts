7cc985a1bb1482e27ff015722c189e03d38b63d0
gavinator9 <epikdude@rocketmail.com>
2023-10-17 01:46:18 -0700

    Did da work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloangular';
+  title = 'hello and welcome to my site';
 }
